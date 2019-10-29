b78204cdc8daec8901ed707ae9a176b923d3ff9f
bdm190556 <57119151+bdm190556@users.noreply.github.com>
2019-10-29 09:21:02 +0700

    Create views.py

---
+++ myproject/learning_logs/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Topic
+
+def home (request):
+    return render (request, 'home.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'topics.html', context)
+
+def topic(request, topic_id):
+    """Выводит одну тему и все ее записи."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'topic.html', context)
